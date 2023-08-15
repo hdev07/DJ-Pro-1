bb544adb30fb01345a741b87933bcac0c70909c6
Alejandro Cruz <hcruz0716@gmail.com>
2023-08-15 15:58:36 -0600

    first commit

---
+++ utils/i18n.ts
@@ -0,0 +1,33 @@
+import i18n from "i18n";
+import { join } from "path";
+import { config } from "./config";
+
+i18n.configure({
+  locales: ["en", "es"],
+  directory: join(__dirname, "..", "locales"),
+  defaultLocale: "es",
+  retryInDefaultLocale: true,
+  objectNotation: true,
+  register: global,
+
+  logWarnFn: function (msg) {
+    console.log(msg);
+  },
+
+  logErrorFn: function (msg) {
+    console.log(msg);
+  },
+
+  missingKeyFn: function (locale, value) {
+    return value;
+  },
+
+  mustacheConfig: {
+    tags: ["{{", "}}"],
+    disable: false
+  }
+});
+
+i18n.setLocale(config.LOCALE);
+
+export { i18n };
